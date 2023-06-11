d8f4f26f34dc0bced9f1a0f26c56a82dc5f53803
Your Name <you@example.com>
2023-06-12 03:04:36 +1200

    adding anomaly detection and clustering

---
+++ Botrytis disease model/pipelines/ml_fit/experiments/anomaly_detection/run.py
@@ -0,0 +1,64 @@
+##############################################
+# Imports
+
+import os
+from os.path import join as path_join
+from pycaret.anomaly import (setup, create_model, plot_model)
+from matplotlib import pyplot as plt
+from pathlib import Path
+import sys
+
+base_dir = "../.."
+sys.path.insert(0, os.path.abspath(base_dir))
+
+from ml_fit_lib.config import get_config
+from ml_fit_lib.constants import *
+from ml_fit_lib.data import Data
+from ml_fit_lib.mlops import MLFlowPipeline
+
+##############################################
+# Constants
+
+if __name__ == "__main__":
+    CONFIG = get_config(CONFIG_PATH)
+
+    PIPELINE = MLFlowPipeline()
+    RUN_DISTRIBUTED = CONFIG.get("run_distributed")
+
+    DATA = Data()
+    
+##############################################
+# Main
+
+def main():
+    tmp_dir = PIPELINE.init(CONFIG)
+    df = DATA.read_csv(DATA_FILE)
+    
+    kwargs = {}
+    for k in ["log_profile", "log_data", "profile", "log_experiment", "log_plots", "use_gpu", "n_jobs", 
+        "normalize_method", "normalize", "low_variance_threshold", "numeric_imputation", "imputation_type", 
+        "ignore_features", "categorical_features"]:
+       kwargs[k] = CONFIG.get(k)
+
+    exp_setup = setup(
+        df, preprocess = True, html = False, session_id = CONFIG.get("random_seed"), 
+        experiment_name = CONFIG.get("MLFLOW_EXPERIMENT_NAME"), **kwargs
+    )
+
+    model = create_model(
+        CONFIG.get("anomaly_model"), fraction = CONFIG.get("contamination_fraction")
+    )
+    
+    for plot in ["tsne", "umap"]:
+        out_file = plot_model(model, plot = plot, save = tmp_dir)
+        out_path = Path(out_file)
+        if out_path.suffix == ".png":
+            out_path = out_path.rename(out_path.with_suffix('.html'))
+        PIPELINE.log_artifact(str(out_path))
+
+    config_out = path_join(tmp_dir, CONFIG_FILE)
+    CONFIG.export(config_out)
+    PIPELINE.log_artifact(config_out)
+
+if __name__ == "__main__":
+    main()
